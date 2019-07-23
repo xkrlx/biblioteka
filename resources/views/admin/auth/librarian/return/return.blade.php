9272c5f59effb3ef2d5d40adf77c40f0d5163337
xkrlx <52510279+xkrlx@users.noreply.github.com>
2019-07-23 12:22:41 +0200

    Library Commit

---
+++ resources/views/admin/auth/librarian/return/return.blade.php
@@ -0,0 +1,37 @@
+@extends('admin.layout.auth')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 ">
+                @include('flash-message')
+                <div class="card">
+                    <div class="card-header">Wypożyczanie książki</div>
+                    <div class="card-body">
+                        <div class="col-12">
+                            {!! Form::open(['route'=>'admin.confirm.booking']) !!}
+                            {!! Form::label('Podaj id użytkownika zwracającego książke:') !!}
+                            {!! Form::number('user_id',null) !!}
+                            @if ($errors->has('user_id'))
+                                <span class="help-block">
+                            <strong>{{ $errors->first('user_id') }}</strong>
+                        </span>
+                            @endif
+                        </div>
+                        <div class="col-12">
+                            {!! Form::submit('Kontunuuj') !!}
+                            {!! link_to(route('admin.index.library'), 'Powrót') !!}
+                        </div>
+
+                        {!! Form::close() !!}
+                    </div>
+
+
+                    <br/>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+@endsection
---
+++ resources/views/admin/auth/librarian/return/return_list.blade.php
@@ -0,0 +1,61 @@
+@extends('admin.layout.auth')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 ">
+                @include('flash-message')
+                <div class="card">
+                    <div class="card-header">Lista wypożyczonych książek</div>
+                    <div class="card-body">
+
+                        <div class="col-12">
+                            <a href="{{route('admin.add.book')}}">Dodaj książkę</a>
+                            <table class="table-responsive">
+                                <thead>
+                                <tr>
+                                    <th>Numer książki</th>
+                                    <th>Tytuł</th>
+                                    <th>Rok wydania</th>
+                                    <th>Autor</th>
+                                    <th>Wydawnictwo</th>
+                                    <th>Opis</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($books as $i =>$book)
+                                    <tr>
+                                        <td>{{$book->id}}</td>
+                                        <td>{{$book->title}}</td>
+                                        <td>{{$book->year}}</td>
+                                        <td>{{$book->author}}</td>
+                                        <td>{{$book->publisher}}</td>
+                                        <td>{{$book->description}}</td>
+                                        @if($book->photo)
+                                            <td><img height="300px" width="405px" class="figure-img" src="/images/covers/{{$book->photo}}"></td>
+                                        @else
+                                            <td>Brak zdjęcia okładki</td>
+                                        @endif
+
+                                        <td>
+                                            <a href="{{route('admin.edit.book',$book)}}">Edytuj</a>
+                                            {{--                                            {!! Form::open(['route'=>['admin.delete.book',$admin],'method'=>'delete']) !!}--}}
+                                            {{--                                            {!! Form::submit('Usuń') !!}--}}
+                                            {{--                                            {!! Form::close() !!}--}}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+
+                            {{$books->links()}}
+
+                            <a href="{{route('admin.index.library'),'Powrót'}}">Powrót</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+

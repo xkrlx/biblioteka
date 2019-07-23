1923904766890132f3cb55e8516bd62374ddf691
xkrlx <52510279+xkrlx@users.noreply.github.com>
2019-07-23 13:29:36 +0200

    Return Books Commit

---
+++ resources/views/admin/auth/librarian/return/available_list.blade.php
@@ -0,0 +1,59 @@
+@extends('admin.layout.auth')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 ">
+                @include('flash-message')
+                <div class="card">
+                    <div class="card-header">
+                        Lista wypożyczonych książek
+                    </div>
+                    <div class="card-body">
+
+                        <div class="col-12">
+                            <a href="{{route('admin.add.book')}}">Dodaj książkę</a>
+                            <table class="table-responsive">
+                                <thead>
+                                <tr>
+                                    <th>Numer książki</th>
+                                    <th>Tytuł</th>
+                                    <th>Rok wydania</th>
+                                    <th>Autor</th>
+                                    <th>Wydawnictwo</th>
+                                    <th>Opis</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($books as $i =>$book)
+                                    <tr>
+                                        <td>{{$book->book_id}}</td>
+                                        <td>{{$book->title}}</td>
+                                        <td>{{$book->year}}</td>
+                                        <td>{{$book->author}}</td>
+                                        <td>{{$book->publisher}}</td>
+                                        <td>{{$book->description}}</td>
+                                        @if($book->photo)
+                                            <td><img height="300px" width="405px" class="figure-img" src="/images/covers/{{$book->photo}}"></td>
+                                        @else
+                                            <td>Brak zdjęcia okładki</td>
+                                        @endif
+
+                                        <td>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+
+                            {{$books->links()}}
+
+                            <a href="{{route('admin.index.library'),'Powrót'}}">Powrót</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
